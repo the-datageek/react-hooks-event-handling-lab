b680c9f4959b1b06864c1be705b42f157f8b8063
Faith Moraa <moraaf770@gmail.com>
2023-01-12 18:44:04 +0300

    Completed Assignemnt

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,13 @@
-// Code EyesOnMe Component Here
+// Code EyesOnMe Component
+
+
+function EyesOnMe(){
+   
+    return (
+        <div>
+            <button onFocus={(e) => {console.log('Good')}} onBlur={(e) => {console.log('Hey! Eyes on me!')}} >Eyes on me</button>
+        </div>
+    )
+}
+
+export default EyesOnMe;
